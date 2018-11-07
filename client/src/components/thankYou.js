92cdeab24d51d27dd99aaf0c5a89927947938540
Michael Burnley <michael@cybercowgames.com>
2018-11-07 09:22:34 -0800

    thank you page update

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const ThankYou = ({ pixelView }) => {
   return(
-    <div>
+    <div id="thank-you-container">
       {pixelView()}
       <h1>Thank you for signing up!</h1>
-      <div><a href="https://drive.google.com/uc?export=download&id=1PS00GEqMjNLuGedrIyQf6My7fcqsj4Uw" download>Download the Holiday Survival Guide</a></div>
-      <div>
-        <img src="/survival_guide_image.jpg" />
+      <div><a href="https://drive.google.com/uc?export=download&id=1PS00GEqMjNLuGedrIyQf6My7fcqsj4Uw" download><button>Download the Holiday Survival Guide</button></a></div>
+      <div id="thank-you-image-wrapper">
+        <a href="https://drive.google.com/uc?export=download&id=1PS00GEqMjNLuGedrIyQf6My7fcqsj4Uw" download>
+          <img src="/survival_guide_image.jpg" />
+        </a>
       </div>
     </div>
   );
