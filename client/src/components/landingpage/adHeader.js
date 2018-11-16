938fd4d26db7cdd5a560f760253232451510d93f
Michael Burnley <michael@cybercowgames.com>
2018-11-16 14:46:03 -0800

    added program dates

--- client/src/components/landingpage/adHeader.js
+++ client/src/components/landingpage/adHeader.js
@@ -3,6 +3,7 @@ import React from 'react';
 const AdHeader = () => (
   <div>
       <h1>Holiday Survival Program</h1>
+      <div className="bold">Starts from 11/26  - 12/31</div>
       <br />
   </div>
 );
