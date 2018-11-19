57c792c2ed460efead0d7fc7d4dd83bdcf011f80
Michael Burnley <micheal.burnley@v1.co>
2018-11-19 07:16:28 -0800

    adding poster and video updates

--- client/src/components/landingpage/adVideo.js
+++ client/src/components/landingpage/adVideo.js
@@ -7,15 +7,17 @@ import { Player } from 'video-react';
 // http://thecamptc.com/videos/SamThanksgiving_1x1_tiny.mp4
 
 
+
+
 const VideoPlayer = (props) => {
   return (
     <div id="video-wrapper">
         <Player
-        playsInline
-        autoPlay={true}
-        muted={false}
-        poster="/videoPoster.png"
-        src="http://thecamptc.com/videos/SamAlejandra_wideToFit_tiny.mp4"
+          playsInline
+          autoPlay={true}
+          muted={false}
+          poster="http://thecamptc.com/videos/videoPoster.PNG"
+          src="http://thecamptc.com/videos/SamAlejandra_wideToFit_tiny.mp4"
         />
     </div>
   );
