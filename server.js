b5dd3e760c700d61a632453f6ec563bc27cc94be
Michael Burnley <micheal.burnley@v1.co>
2019-01-16 16:50:16 -0800

    added https enforcement fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const Timber = require('./src/timber');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000; 
 const helpers = require('./src/helpers');
-const enfore = require('express-sslify')
+const enforce = require('express-sslify')
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
