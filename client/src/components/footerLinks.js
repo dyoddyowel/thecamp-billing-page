659a372004f287540effd2558b35865b8fe42a45
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-06-03 23:47:22 +0800

    Update footerLinks.js

--- client/src/components/footerLinks.js
+++ client/src/components/footerLinks.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FooterLinks = () => (
     <div id="footerlinks">
         <a href="http://www.thecamptc.com/privacy" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
-        <a href="mailto: michael@onepercentnutrition.com">Support</a>
+        <a href="mailto: jowel@thecamptc.com">Support</a>
     </div>
 );
 
