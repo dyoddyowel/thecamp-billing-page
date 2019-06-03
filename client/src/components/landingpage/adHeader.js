e24a9b3c85db5114213f1577aac4aae0a97ac8db
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-06-03 23:20:57 +0800

    Update adHeader.js

--- client/src/components/landingpage/adHeader.js
+++ client/src/components/landingpage/adHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AdHeader = () => (
   <div>
-      <h1>21 Day Summer Shred</h1>
-      <div className="bold">Starts from 3/18 - 4/7</div>
+      <h1>Summer Sizzle Slim Down</h1>
+      <div className="bold">Starts from 6/247</div>
       <br />
   </div>
 );
