302c7e59ebb849a5bb50850dc80be7bbcbea2494
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-06-05 07:40:50 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -98,7 +98,7 @@ class App extends Component {
       this.nextSection();
       ReactPixel.track('Purchase', {
         'currency': 'USD',
-        'value': 97.0
+        'value': 37.0
       });
     } else {
       this.setState({ error: true });
