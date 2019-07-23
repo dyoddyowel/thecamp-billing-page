b5c7ed1b8af17e70892bdca05eaedd0370627985
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-07-24 05:34:09 +0800

    Update adCopy.js

--- client/src/components/landingpage/adCopy.js
+++ client/src/components/landingpage/adCopy.js
@@ -5,11 +5,10 @@ const AdCopy = ({ id }) => (
         <div id={id}>
           <ul>
             <li>Unlimited Camp Classes</li>
-            <li>27 Page Nutrition Guide With Recipes</li>
+            <li>22 Page Nutrition Guide With Recipes</li>
             <li>Nutritional Seminar</li>
             <li>Weekly Weigh-Ins</li>
             <li>Support and Guidance</li>
-            <li style={{fontWeight: 'bold'}}>* Only Available for New Members At Participating Locations</li>
           </ul>
         </div>
   </div>
