a4e2ee25938b797c8dd5d4faa02506f6f7e7239b
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-07-25 21:58:19 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -5,9 +5,9 @@ const ThankYou = ({ pixelView }) => {
     <div id="thank-you-container">
       {pixelView()}
       <h1>Thank you for signing up!</h1>
-      <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="mailto: jowel@thecamptc.com">contact us</a>.</div>
+      <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://www.thecamptc.com/locations.php">contact us</a>.</div>
       <div id="thank-you-image-wrapper">
-        <div><a href="/Summer_Sizzle_Slim_Down_MANUAL.pdf" download><button>Download the Summer Sizzle Slim Down Manual</button></a></div>
+        <div><a href="/21-Day-Back-to-School-Macro-Counting-Program.pdf" download><button>Download the 21 Day Back to School Macro Counting Program Manual</button></a></div>
       </div>
     </div>
   );
