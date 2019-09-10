5eee8d66ea38a08cda6f2a828e8e331c0ceafe8b
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-09-11 06:36:32 +0800

    Update adVideo.js

--- client/src/components/landingpage/adVideo.js
+++ client/src/components/landingpage/adVideo.js
@@ -2,13 +2,6 @@ import React from 'react';
 import "../../video-react.css"; // import css
 import { Player } from 'video-react';
 
-// http://thecamptc.com/videos/AlexandraThanksgiving_1x1_tiny.mp4
-// http://thecamptc.com/videos/SamAlejandra_wideToFit_tiny.mp4
-// http://thecamptc.com/videos/SamThanksgiving_1x1_tiny.mp4
-
-
-
-
 const VideoPlayer = (props) => {
   return (
     <div id="video-wrapper">
@@ -17,7 +10,7 @@ const VideoPlayer = (props) => {
           poster='/videoPoster.PNG'
           autoPlay={false}
           muted={false}
-          src="http://www.thecamptc.com/videos/macro.mp4"
+          src="https://www.thecamptc.com/videos/fallback.mp4"
         />
     </div>
   );
