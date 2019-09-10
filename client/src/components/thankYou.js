c04beb59526ceae8ccd34a4e42ae63a8d85440ec
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-09-11 06:22:59 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -7,7 +7,7 @@ const ThankYou = ({ pixelView }) => {
       <h1>Thank you for signing up!</h1>
       <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://www.thecamptc.com/locations.php">contact us</a>.</div>
       <div id="thank-you-image-wrapper">
-        <div><a href="/21-Day-Back-to-School-Macro-Counting-Program.pdf" download><button>Download the 21 Day Back to School Macro Counting Program Manual</button></a></div>
+        <div><a href="/fallback-manual.pdf" download><button>Download the Fall Back Into Fitness 21 Day Detox Program Manual</button></a></div>
       </div>
     </div>
   );
