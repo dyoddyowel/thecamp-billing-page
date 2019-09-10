1fa03f3580e17e3db25ddb10e49b2cf04e468c10
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-09-11 06:17:29 +0800

    Update headerBanner.js

--- client/src/components/landingpage/headerBanner.js
+++ client/src/components/landingpage/headerBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HeaderBanner = () => (
-  <div id="header-banner">21 Day Back to School Macro Counting</div>
+  <div id="header-banner">Fall Back Into Fitness 21 Day Detox</div>
 );
 
 export default HeaderBanner;
