b70d7693bda27b760bb94df263442be14cc61ff8
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2019-10-30 04:48:57 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -7,7 +7,7 @@ const ThankYou = ({ pixelView }) => {
       <h1>Thank you for signing up!</h1>
       <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://www.thecamptc.com/locations.php">contact us</a>.</div>
       <div id="thank-you-image-wrapper">
-        <div><a href="/Fall-Back-Into-Fitness-21-Day-Detox-Manual.pdf" download><button>Download the Fall Back Into Fitness 21 Day Detox Program Manual</button></a></div>
+        <div><a href="/Gobble-The-Pounds-Manual.pdf" download><button>Download the Gobble The Pounds Away 21 Day Carb Cycling Program Manual</button></a></div>
       </div>
     </div>
   );
