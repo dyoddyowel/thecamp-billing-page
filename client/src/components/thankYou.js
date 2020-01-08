d2c67957cef1fc324e6c36953180b56034bd4a10
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-01-09 04:20:07 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -5,9 +5,9 @@ const ThankYou = ({ pixelView }) => {
     <div id="thank-you-container">
       {pixelView()}
       <h1>Thank you for signing up!</h1>
-      <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://www.thecamptc.com/locations.php">contact us</a>.</div>
+      <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://thecamptc.com/locations">contact us</a>.</div>
       <div id="thank-you-image-wrapper">
-        <div><a href="/Gobble-The-Pounds-Manual.pdf" download><button>Download the Gobble The Pounds Away 21 Day Carb Cycling Program Manual</button></a></div>
+        <div><a href="/tough-love-manual-2020.pdf" download><button>Download the 21 Day Tough Love 2020 Program Manual</button></a></div>
       </div>
     </div>
   );
