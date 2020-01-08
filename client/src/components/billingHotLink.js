ac5f659650d21c1dd09d031a2de1439608c514fd
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-01-09 04:17:19 +0800

    Update billingHotLink.js

--- client/src/components/billingHotLink.js
+++ client/src/components/billingHotLink.js
@@ -42,8 +42,8 @@ const BillingPage = ({
 }) => (
   <React.Fragment>
     <PromoDetails
-      detailname="Gobble The Pounds Away 21 Day Carb Cycling"
-      price="47" />
+      detailname="21 Day Tough Love 2020"
+      price="67" />
     <Video />
     <AdCopy
       id="billing-copy" />
@@ -113,7 +113,7 @@ class BillingHotLink extends Component {
           "BillingPostalCode": "",
       },
       payment: {
-          "amount": 47,
+          "amount": 67,
           "number": "",
           "expiry": "",
           "cvc": "",
@@ -212,7 +212,7 @@ class BillingHotLink extends Component {
       this.nextSection();
       ReactPixel.track('Purchase', {
         'currency': 'USD',
-        'value': 47.0
+        'value': 67.0
       });
       this.setState({ complete: true });
     } else {
