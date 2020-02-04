3532f774f53d31c2ff737a7745bf5fa22c742620
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 01:02:31 +0800

    Update headerBanner.js

--- client/src/components/landingpage/headerBanner.js
+++ client/src/components/landingpage/headerBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HeaderBanner = () => (
-  <div id="header-banner">21 Day Tough Love 2020</div>
+  <div id="header-banner">16 Week Challenge</div>
 );
 
 export default HeaderBanner;
