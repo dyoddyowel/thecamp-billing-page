341cb646731da2d515894ddd458e502ba1e98de2
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 01:01:11 +0800

    Update adCopy.js

--- client/src/components/landingpage/adCopy.js
+++ client/src/components/landingpage/adCopy.js
@@ -4,12 +4,14 @@ const AdCopy = ({ id }) => (
   <div>
         <div id={id}>
           <ul>
-            <li>Unlimited Camp Classes</li>
-            <li>Nutrition Guide With Recipes</li>
-            <li>Nutritional Seminar</li>
-            <li>Weekly Weigh-Ins</li>
-            <li>Support and Guidance</li>
+	    		  <li>Do you want to Drop the gut and build muscle and strength?</li>
+			      <li>Do you have a nutrition plan customised to your lifestyle?</li>
+			      <li>Do you have unlimited sessions each week to get results?</li>
+			      <li>Do you improve your health and fitness?</li>
+			      <li>Do you want to get in the best shape of your life?</li>
+			      <li>Do you want a community supporting you to your goals every step of the way?</li>
           </ul>
+          <p>If you answered yes to the above then you fit the criteria to join our Brand New 16 Week Challenge today.</p>
         </div>
   </div>
 );
