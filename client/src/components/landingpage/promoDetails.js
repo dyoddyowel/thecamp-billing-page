20e0d9cc7b70c934271a6145e630ffc2aa166502
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 01:26:34 +0800

    Update promoDetails.js

--- client/src/components/landingpage/promoDetails.js
+++ client/src/components/landingpage/promoDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PromoDetails = ({ detailname, price }) => (
     <div className="details">
-       <span className="highlight">${price}</span> for the <span className="highlight">{ detailname }</span> program, will get you:
+       <span className="highlight">${price}</span> for the <span className="highlight">{ detailname }</span> program will get you:
     </div>
 );
 
