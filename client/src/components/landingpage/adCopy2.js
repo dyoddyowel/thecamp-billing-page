17aa65e0d023b225dd94a6c5f214cf7fa6aa03fe
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 00:48:32 +0800

    Create adCopy2.js

---
+++ client/src/components/landingpage/adCopy2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AdCopy = ({ id }) => (
+  <div>
+        <div id={id}>
+          <ul>
+            <li>Unlimited Camp Classes</li>
+            <li>Nutrition Guide With Recipes</li>
+            <li>Nutritional Seminar</li>
+            <li>Weekly Weigh-Ins</li>
+            <li>Support and Guidance</li>
+          </ul>
+        </div>
+  </div>
+);
+
+export default AdCopy;
