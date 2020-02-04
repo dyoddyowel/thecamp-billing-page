e1cf97dab786456dbcbd2d66bb33a347be8d9cae
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 04:11:24 +0800

    Update promoDetails.js

--- client/src/components/landingpage/promoDetails.js
+++ client/src/components/landingpage/promoDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PromoDetails = ({ detailname, price }) => (
     <div className="details">
-       <span className="highlight">${price}</span> for the <span className="highlight">{ detailname }</span> program will get you:
+       We are excited to announce our brand new <span className="highlight">{ detailname }</span> for only <span className="highlight">${price}</span>
     </div>
 );
 
