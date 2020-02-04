76c060a618dbe96c85ebb8042d14e5eae3163768
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-05 01:19:02 +0800

    Update adCopy.js

--- client/src/components/landingpage/adCopy.js
+++ client/src/components/landingpage/adCopy.js
@@ -4,14 +4,14 @@ const AdCopy = ({ id }) => (
   <div>
         <div id={id}>
           <ul>
-	    		  <li>Do you want to Drop the gut and build muscle and strength?</li>
-			      <li>Do you have a nutrition plan customised to your lifestyle?</li>
-			      <li>Do you have unlimited sessions each week to get results?</li>
-			      <li>Do you improve your health and fitness?</li>
-			      <li>Do you want to get in the best shape of your life?</li>
-			      <li>Do you want a community supporting you to your goals every step of the way?</li>
+		<li>Do you want to Drop the gut and build muscle and strength?</li>
+		<li>Do you have a nutrition plan customised to your lifestyle?</li>
+		<li>Do you have unlimited sessions each week to get results?</li>
+		<li>Do you improve your health and fitness?</li>
+		<li>Do you want to get in the best shape of your life?</li>
+		<li>Do you want a community supporting you to your goals every step of the way?</li>
           </ul>
-          <p>If you answered yes to the above then you fit the criteria to join our Brand New 16 Week Challenge today.</p>
+          <p style="font-size=24px;font-weight=500">If you answered yes to the above then you fit the criteria to join our Brand New 16 Week Challenge today.</p>
         </div>
   </div>
 );
