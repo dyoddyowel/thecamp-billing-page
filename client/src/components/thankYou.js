377f88842778d3e685a2602aebb163a7b0242a37
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-27 04:20:28 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -6,6 +6,9 @@ const ThankYou = ({ pixelView }) => {
       {pixelView()}
       <h1>Thank you for signing up!</h1>
       <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://thecamptc.com/locations">contact us</a>.</div>
+      <div id="thank-you-image-wrapper">
+        <div><a href="/xxx.pdf" download><button>Download the Spring Cleaning Detox Program Manual</button></a></div>
+      </div>
     </div>
   );
 }
