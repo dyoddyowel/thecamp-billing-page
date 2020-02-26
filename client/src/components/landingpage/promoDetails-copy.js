90b823ba4c5df1b9b44fac24945e76a86ff7472d
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-02-27 04:17:55 +0800

    Delete promoDetails-copy.js

--- client/src/components/landingpage/promoDetails-copy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PromoDetails = ({ detailname, price }) => (
-    <div className="details">
-       <span className="highlight">${price}</span> for the <span className="highlight">{ detailname }</span> program will get you:
-    </div>
-);
-
-export default PromoDetails;
