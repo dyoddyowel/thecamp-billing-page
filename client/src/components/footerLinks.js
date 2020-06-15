a81557ad24ca1f11354e1fa688334b0df5565b1d
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-06-16 01:02:16 +0800

    Update footerLinks.js

--- client/src/components/footerLinks.js
+++ client/src/components/footerLinks.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const FooterLinks = () => (
     <div id="footerlinks">
-        <a href="https://www.thecamptc.com/privacy" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
-        <a href="https://thecamptc.com/locations">Contact Us</a>
+        <a href="https://thecamptc.com/about/privacy-policy" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+        <a href="https://thecamptc.com/locations" target="_blank">Contact Us</a>
     </div>
 );
 
