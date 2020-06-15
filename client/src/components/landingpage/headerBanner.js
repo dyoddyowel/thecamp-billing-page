8962091d988d65a62e22f5ca908e0fa01d01c9ca
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-06-16 01:03:25 +0800

    Update headerBanner.js

--- client/src/components/landingpage/headerBanner.js
+++ client/src/components/landingpage/headerBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HeaderBanner = () => (
-  <div id="header-banner">21 Day Summer Sizzle Intermittent Fasting</div>
+  <div id="header-banner">21 Day Summer Sizzle</div>
 );
 
 export default HeaderBanner;
