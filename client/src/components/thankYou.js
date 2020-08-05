3e79141132006405812e54ae6ba5d7548b696e35
dyoddyowel <50218372+dyoddyowel@users.noreply.github.com>
2020-08-06 02:39:55 +0800

    Update thankYou.js

--- client/src/components/thankYou.js
+++ client/src/components/thankYou.js
@@ -7,7 +7,7 @@ const ThankYou = ({ pixelView }) => {
       <h1>Thank you for signing up!</h1>
       <div>We look forward to see you at the program's start! You will receive more information to your provided email in the coming days. If you have any questions, please <a href="https://thecamptc.com/locations">contact us</a>.</div>
       <div id="thank-you-image-wrapper">
-        <div><a href="/Summer-Sizzle-Manual.pdf" download><button>Download the Summer Sizzle Program Manual</button></a></div>
+        <div><a href="/shakeitoff.pdf" download><button>Download the Shake It Off Program Manual</button></a></div>
       </div>
     </div>
   );
